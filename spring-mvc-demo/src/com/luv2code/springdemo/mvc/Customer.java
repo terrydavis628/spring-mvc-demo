c6e96a699b9320e1d88063123900ea2ad5d18f32
Terry Davis <davist15@email.franklin.edu>
2018-01-15 01:03:49 -0500

    Initial checkin

---
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -0,0 +1,54 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.springframework.beans.factory.annotation.Required;
+
+public class Customer {
+	
+	private String firstName;
+	
+	@NotNull(message="Required")
+	@Size(min=1, message="Required")
+	private String lastName;
+	
+	@Min(value=0, message="must be greater than or equal to zero")
+	@Max(value=10, message="must be less than or equal to ten")
+	@NotNull(message="Required")
+	private Integer freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	
+}
